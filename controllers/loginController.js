eadb21c2229e80bcf82eb702b2eea360eac55a8e
Rahul Agarwal <rahulagarwal0605@gmail.com>
2021-11-22 08:08:03 +0530

    Added login controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,18 +4,38 @@ require("dotenv").config();
 const jwt = require('jsonwebtoken');
 const db = require('../config/db');
 const sendToken = require('../util/jwtToken');
+const bcrypt = require('bcrypt');
 
 exports.login = (req, res) => {
-  // authenticate user
-  // assign id in variable userID
-  const user = {id:userID};
-  sendToken(user,res);
+  var username = req.body.username;
+  var password = req.body.password;
+  bcrypt.hash(password, 10, (err, encrypted) => {
+    password = encrypted
+  })
+	db.query(
+		"SELECT * FROM Login where username=? and password=?",
+		[username, password],
+		(err, rows) => {
+			if (!err) {
+				if (rows[0]) {
+          const user = {id:rows[0].userID, type:rows[0].userType};
+          sendToken(user,res);
+					res.json({status: "success", data: user, message: "Login successful"});
+				} else {
+					res.json({status: "error", data: null, message: "Invalid user details"});
+				}
+			} else {
+				console.log(err);
+			}
+		}
+	);
 };
 
 exports.logout = (req, res) => {
   res.cookie('token',null,{expires:new Date(Date.now()), httpOnly:true });
   res.status(200).json({
-    success:true,
-    message:'Logged out'
+    success: "success",
+	data: null,
+    message: 'Logged out'
   });
 };
