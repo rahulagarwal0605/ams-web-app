819cf66449a78e00787579394808e88719767dd6
Rahul Agarwal <rahulagarwal0605@gmail.com>
2021-11-22 12:05:44 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,19 +1,11 @@
 const mysql = require("mysql2");
 
 // Connection Creation
-// const db = mysql.createPool({
-// 	host: process.env.DATABASE_HOST,
-// 	user: process.env.DATABASE_USER,
-// 	password: process.env.DATABASE_PASSWORD,
-// 	database: process.env.DATABASE,
-// 	multipleStatements: true,
-// });
-
 const db = mysql.createPool({
-	host: "us-cdbr-east-04.cleardb.com",
-	user: "b59d1fb4b16184",
-	password: "6b103a8f",
-	database: "heroku_ce068efd190b24d",
+	host: process.env.DATABASE_HOST,
+	user: process.env.DATABASE_USER,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE,
 	multipleStatements: true,
 });
 
