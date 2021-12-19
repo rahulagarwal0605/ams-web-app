b9ad785479f1b7a60c334bec87245b1e21c894a6
Rahul Agarwal <rahulagarwal0605@gmail.com>
2021-12-19 20:43:50 +0530

    updates

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -72,7 +72,6 @@ exports.setMarks = (req, res) => {
           // All Error handling will be done later
           if(err)
             console.log(err);
-
         });
       });
       res.json({
@@ -84,11 +83,11 @@ exports.setMarks = (req, res) => {
 };
 
 exports.setGrades = (req, res) => {
-
+  
 };
 
 exports.getEvaluationScheme = (req, res) => {
-    var query="SELECT Exams.ExamName, Exams.ExamDate, Exams.MaximumMarks, Exams.Weightage  FROM Exams inner join Takes on Takes.ExamID=Exams.ExamID WHERE Takes.CourseID =?";
+    var query="SELECT DISTINCT Exams.ExamId, Exams.ExamName, Exams.ExamDate, Exams.TotalMarks, Exams.Weightage  FROM Exams inner join Takes on Takes.ExamID=Exams.ExamID WHERE Takes.CourseID =?";
     db.query(query,[req.params.cid],(err,results) => {
       // All Error handling will be done later
         if(err){
@@ -104,22 +103,108 @@ exports.getEvaluationScheme = (req, res) => {
     );
   };
 
-exports.setEvaluationSceheme = (req, res) => {
-    var query="Insert into Exams(ExamName, ExamDate, MaximumMarks, Weightage) values (?, ?, ?, ?)";
-    db.query(query,[cid,sid],(err,results) => {
+
+
+function setExam(ExamName, MaximumMarks, Weightage, cid, res, callback) {
+  var query="Insert into Exams(ExamName, TotalMarks, Weightage) values (?, ?, ?); Select ExamId from Exams order by ExamId desc limit 1";
+    db.query(query,[ExamName, MaximumMarks, Weightage],(err,results) => {
       // All Error handling will be done later
         if(err){
           console.log(err);
         }
         if(results!=undefined){
+          callback(results[1][0].ExamId, cid, res, setTakes);
+        }
+      }
+    );
+}
+
+function getStudentList(examId, cid, res, callback) {
+  var query="SELECT Student.RollNo FROM Enrolled inner join Student on Enrolled.RollNo=Student.RollNo WHERE Enrolled.CourseID =?";
+    db.query(query,[cid],(err,results) => {
+        // All Error handling will be done later
+        if(err){
+            console.log(err);
+        }
+        if(results!=undefined){
+          callback(examId, cid, res, results);
+        }
+      }
+    );
+}
+
+function setTakes(examId, cid, res, results) {
+  let takes = [];
+  for(let i=0; i<results.length; i++) {
+    takes.push([cid, examId, results[i].RollNo, 0]);
+  }
+  var query="Insert into Takes(CourseId, ExamId, RollNo, MarksObtained) values ?";
+    db.query(query,[takes],(err,results) => {
+      // All Error handling will be done later
+        if(err){
+            console.log(err);
+            res.json({
+              status: "error",
+              data:null,
+              message:"Error occured"
+            });
+        }
+        else {
           res.json({
             status: "success",
-            data:results,
+            data:null,
+            message:"Evaluation Scheme set successfully"
           });
         }
       }
     );
-  };
+}
+
+exports.setEvaluationSceheme = (req, res) => {
+  setExam(req.body.ExamName, req.body.MaximumMarks, req.body.Weightage, req.params.cid, res, getStudentList);
+};
+
+exports.editEvaluationSceheme = (req, res) => {
+  var query="update into Exams set ExamName = ?, TotalMarks = ?, Weightage = ? WHERE ExamId =?";
+    db.query(query,[req.body.ExamName, req.body.MaximumMarks, req.body.Weightage, req.body.ExamID],(err,results) => {
+        // All Error handling will be done later
+        if(err){
+            console.log(err);
+        }
+        if(results!=undefined){
+          callback(examId, cid, res, results);
+        }
+      }
+    );
+};
+
+exports.deleteEvaluationSceheme = (req, res) => {
+  var query="delete from Takes where ExamId = ?";
+    db.query(query,[req.body.ExamId],(err,results) => {
+        // All Error handling will be done later
+        if(err){
+            console.log(err);
+        }
+        else {
+          query="delete from Exams where ExamId = ?";
+          db.query(query,[req.body.ExamId],(err,results) => {
+              // All Error handling will be done later
+              if(err){
+                  console.log(err);
+              }
+              else {
+                res.json({
+                  status: "success",
+                  data:null,
+                  message:"Exam component deleted successfully"
+                });
+              }
+            }
+          );
+        }
+      }
+    );
+};
 
 /*
 select*from Takes inner join Exams on
--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -2,13 +2,15 @@
 const express = require("express");
 const router = express.Router();
 const teacherController = require("../controllers/teacherController");
-const {isAuthenticated,authorizeRoles}= require("../middlewares/auth");
+//const {isAuthenticated,authorizeRoles}= require("../middlewares/auth");
 
-router.get("/courses", isAuthenticated,authorizeRoles("Instructor"),teacherController.viewCoursesList);
-router.get("/courses/:cid/students",isAuthenticated,authorizeRoles("Instructor"),teacherController.viewStudentsList);
-router.get("/courses/:cid/EvalutaionScheme",isAuthenticated,authorizeRoles("Instructor"), teacherController.getEvaluationScheme);
-router.post("/courses/:cid/EvalutaionScheme",isAuthenticated,authorizeRoles("Instructor"), teacherController.setEvaluationSceheme);
-router.get("/courses/:cid/students/:sid/getMarks",isAuthenticated,authorizeRoles("Instructor"), teacherController.getMarks);
-router.post("/courses/:cid/students/:sid/setMarks",isAuthenticated,authorizeRoles("Instructor"),teacherController.setMarks);
-router.post("/courses/:cid/students/:sid/setGrades",isAuthenticated,authorizeRoles("Instructor"), teacherController.setGrades);
+router.get("/courses", teacherController.viewCoursesList);
+router.get("/courses/:cid/students",teacherController.viewStudentsList);
+router.get("/courses/:cid/EvalutaionScheme", teacherController.getEvaluationScheme);
+router.delete("/courses/:cid/EvalutaionScheme", teacherController.deleteEvaluationSceheme);
+router.post("/courses/:cid/EvalutaionScheme", teacherController.setEvaluationSceheme);
+router.put("/courses/:cid/EvalutaionScheme", teacherController.editEvaluationSceheme);
+router.get("/courses/:cid/students/:sid/getMarks", teacherController.getMarks);
+router.post("/courses/:cid/students/:sid/setMarks",teacherController.setMarks);
+router.post("/courses/:cid/students/:sid/setGrades", teacherController.setGrades);
 module.exports = router;
